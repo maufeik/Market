cdcde3aed7631ca0210d5213a8146b72bc29197a
Mau Feik <68160228+maufeik@users.noreply.github.com>
2025-01-22 20:36:33 +0100

    Second update

--- shopCart.java
+++ shopCart.java
@@ -15,5 +15,38 @@ public class shopCart {
         price = new double[maxCart];
         contProduct=0;
     }
-    
+    public boolean addElement(String product, int numProd, double price){
+        boolean res= false;
+        if(contProduct<maxCart){
+            this.product[contProduct]=product;
+            this.numProd[contProduct]=numProd;
+            this.price[contProduct]=price;
+            contProduct++;
+            res=true;
+        }
+        return res;
+    }
+    public void buy(){
+        showCart();
+        System.out.println("TOTAL");
+        double total=0.0;
+        for (int i = 0; i < contProduct; i++) {
+            total+=(numProd[i]*price[i]);
+        }
+        System.out.println(String.format("%.2f", total));
+        
+    }
+    public void showCart(){
+        for (int i = 0; i < contProduct; i++) {
+            showElementOfStock(i, product[i], numProd[i], price[i]);
+        }
+    }
+    private void showElementOfStock(int i, String name, int num, double price) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(i).append("--")
+                .append(name).append("--")
+                .append(num).append("--")
+                .append(String.format("%.2f", price*num));
+        System.out.println(sb.toString());
+    }
 }
