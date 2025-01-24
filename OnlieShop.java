4ad91025204e5ebc4ddb0745241f706a639952a4
Mau Feik <68160228+maufeik@users.noreply.github.com>
2025-01-24 16:47:37 +0100

    zzz

--- OnlieShop.java
+++ OnlieShop.java
@@ -1,12 +1,38 @@
 
 package SUper;
 
+import java.util.Scanner;
+
 public class OnlieShop {
 
+    private Stock stock;
+    private shopCart cart;
+
     public static void main(String[] args) {
-        Stock stock = new Stock();
+        OnlieShop os = new OnlieShop();
+        os.run();
+    }
+
+    public OnlieShop() {
+
+        stock = new Stock();
         stock.initStock();
+        cart = new shopCart(3);
+    }
+
+    public void run() {
+        
         stock.showStock();
+        cart.showCart();
     }
     
+    public int showSelectMenu(){
+        int option;
+        Scanner teclado = new Scanner(System.in);
+        String [] options = {"Exit","Show stock","Add product to cart","Show cart","Buy","Fill stock"};
+        for (int i = 0; i < options.length; i++) {
+            System.out.println(i+" "+options[i]);            
+        }
+        return option=teclado.nextInt();
+    }
 }
